71fc48c7b44b7b1e1ff6bec9b53ecc3f003b25af
Levi Miller <paullevimiller@gmail.com>
2019-11-18 16:29:46 -0800

    Conditionally render component

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import BotCollection from "./BotCollection";
+import BotSpecs from "../components/BotSpecs";
 import YourBotArmy from "./YourBotArmy";
 
 const url = "https://bot-battler-api.herokuapp.com/api/v1/bots";
@@ -7,7 +8,8 @@ const url = "https://bot-battler-api.herokuapp.com/api/v1/bots";
 class BotsPage extends React.Component {
   state = {
     allBots: [],
-    yourBots: []
+    yourBots: [],
+    currentBot: null
   };
 
   componentDidMount() {
@@ -16,33 +18,42 @@ class BotsPage extends React.Component {
       .then( json =>  this.setState({allBots: json}) );
   };
 
-  handleClickBot = (botCardProps) => {
-    // console.log(botCardProps);
+  inspectBot = botCardProps => this.setState({currentBot: botCardProps.bot.id});
+  
+  dismissBot = botCardProps => {
     let yourBots = this.state.yourBots;
     let allBots = this.state.allBots;
-    let indexOfBot;
-    if (botCardProps.enlisted === false) {
-      yourBots.push(botCardProps.bot);
-      indexOfBot = allBots.indexOf(botCardProps.bot);
-      allBots.splice(indexOfBot, 1);
-      this.setState({  allBots: allBots, yourBots: yourBots });
-      // return botCardProps.bot;
-    } else {
-      indexOfBot = yourBots.indexOf(botCardProps.bot);
-      yourBots.splice(indexOfBot, 1);
-      allBots.push(botCardProps.bot);
-      this.setState({  allBots: allBots, yourBots: yourBots });
-      // return botCardProps.bot;
-    };
+    let index = this.state.yourBots.indexOf(botCardProps.bot);
+    yourBots.splice(index, 1);
+    allBots.push(botCardProps.bot);
+    this.setState({ allBots: allBots, yourBots: yourBots });
   };
 
+  enlistBot = botCardProps => {
+    let yourBots = this.state.yourBots;
+    let allBots = this.state.allBots;
+    let index = this.state.allBots.indexOf(botCardProps.bot);
+    allBots.splice(index, 1);
+    yourBots.push(botCardProps.bot);
+    this.setState({ allBots: allBots, yourBots: yourBots });
+    this.setState({ currentBot: null });
+  };
 
+  goBack = () => this.setState({ currentBot: null });
 
   render() {
+    let view;
+    const currentBot = this.state.allBots.find(bot => bot.id === this.state.currentBot);
+    if (!currentBot) {
+      view = (<BotCollection bots={this.state.allBots} handleClickBot={this.inspectBot} />);
+    } else {
+      view = (<BotSpecs bot={currentBot} handleEnlist={this.enlistBot} handleGoBack={this.goBack} /> );
+    };
+
     return (
       <div>
-        <YourBotArmy bots={this.state.yourBots} handleClickBot={this.handleClickBot} />
-        <BotCollection bots={this.state.allBots} handleClickBot={this.handleClickBot} />
+        <YourBotArmy bots={this.state.yourBots} handleClickBot={this.dismissBot} />
+        { view }
       </div>
     );
   };
